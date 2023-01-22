7f692a9a7e0e92503f2bff095c6d1b8ef489b13a
Ruben Amarilla <daniel.amarilla.rda@gmail.com>
2023-01-22 05:23:10 -0300

    gran inversion version

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import PersonCard from './components/personCard';
 
 function App() {
   return (
-    <div>
-      <PersonCard name={"Jane"} lastName={"Doe"} age={"45"} hairColor={"Gray"}/>
-      <PersonCard name={"John"} lastName={"Smith"} age={"18"} hairColor={"Brown"}/>
-      <PersonCard name={"Millard"} lastName={"Fillmore"} age={"25"} hairColor={"Black"}/>
-      <PersonCard name={"Maria"} lastName={"Smith"} age={"36"} hairColor={"Blonde"}/>
+    <div className='App'>
+      <PersonCard name={"Jane"} lastName={"Doe"} age={45} hairColor={"Gray"}/>
+      <PersonCard name={"John"} lastName={"Smith"} age={18} hairColor={"Brown"}/>
+      <PersonCard name={"Millard"} lastName={"Fillmore"} age={25} hairColor={"Black"}/>
+      <PersonCard name={"Maria"} lastName={"Smith"} age={36} hairColor={"Blonde"}/>
     </div>
     
   );
--- src/components/personCard.jsx
+++ src/components/personCard.jsx
@@ -1,18 +1,13 @@
 import React from "react";
-class PersonCard extends React.Component {
-    render () {
-        let myStyle = {
-            margin: "5px auto",
-        }
-        let { name, lastName, age, hairColor } = this.props;
-        return (
-            <div className="container" style={myStyle}>
-                <h1>{ lastName }, { name }</h1>
-                <p>Age: { age }</p>
-                <p>Hair Color: { hairColor }</p>
-            </div>
-        );
-    }
+
+const PersonCard = props => {
+    return (
+        <div>
+            <h1>{ props.lastName }, { props.name }</h1>
+            <p>Age: { props.age }</p>
+            <p>Hair Color: { props.hairColor }</p>
+        </div>
+    );
 }
 
 export default PersonCard;
